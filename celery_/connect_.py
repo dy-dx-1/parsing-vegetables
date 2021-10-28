82efb2dfc4d3849ba8ef249f4e25d15bb64ed6df
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2021-10-28 19:35:51 -0400

    cleared up path

--- celery_/connect_.py
+++ celery_/connect_.py
@@ -2,11 +2,11 @@ import pymysql
 from assets import Connect
 
 def get_all_data(): 
-    with open(r"C:\Users\Nicolas\Desktop\sql_login.txt", "r") as file:
+    with open(r"..\sql_login.txt", "r") as file:
         l_cred = [line[:-1] for line in file] 
     l_cred[3] = int(l_cred[3]) 
+    
     celery_d = Connect(l_cred[0], l_cred[1], l_cred[2], l_cred[3], l_cred[4]).return_info()
-
     connection = pymysql.connect(user = celery_d[0], passwd = celery_d[1], host = celery_d[2], port = celery_d[3],
     database = celery_d[4], cursorclass=pymysql.cursors.DictCursor) 
 
